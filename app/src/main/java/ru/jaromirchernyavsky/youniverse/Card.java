4765ba4b88cfa8c643d388a1d7229103fd668f78
Pink Lasagna <80715845+Pink-Lasagna@users.noreply.github.com>
2024-05-14 15:46:13 +0300

    Initial commit

---
+++ app/src/main/java/ru/jaromirchernyavsky/youniverse/Card.java
@@ -0,0 +1,25 @@
+package ru.jaromirchernyavsky.youniverse;
+
+import android.net.Uri;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Base64;
+
+import java.util.Arrays;
+
+public class Card {
+    String data;
+    JSONObject convertedData;
+    Uri uri;
+    String name;
+    String description;
+    public Card(String data, Uri uri) throws JSONException {
+        this.data = data;
+        this.uri = uri;
+        convertedData = new JSONObject(new JSONObject(new String(Base64.decode(data, Base64.DEFAULT))).getString("data"));
+        name = convertedData.getString("name");
+        description = convertedData.getString("description");
+    }
+}
--- app/src/main/java/ru/jaromirchernyavsky/youniverse/ui/home/HomeFragment.java
+++ app/src/main/java/ru/jaromirchernyavsky/youniverse/ui/home/HomeFragment.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import org.json.JSONException;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import ar.com.hjg.pngj.IImageLine;
 import ar.com.hjg.pngj.PngReader;
@@ -20,36 +26,53 @@ import ar.com.hjg.pngj.PngWriter;
 import ar.com.hjg.pngj.chunks.ChunkCopyBehaviour;
 import ar.com.hjg.pngj.chunks.PngChunkTEXT;
 import ar.com.hjg.pngj.chunks.PngChunkTextVar;
+import ru.jaromirchernyavsky.youniverse.Card;
+import ru.jaromirchernyavsky.youniverse.MainAdapter;
+import ru.jaromirchernyavsky.youniverse.R;
 import ru.jaromirchernyavsky.youniverse.databinding.FragmentCardBinding;
 import ru.jaromirchernyavsky.youniverse.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    private ArrayList<Card> cards = new ArrayList<Card>();
     private FragmentHomeBinding binding;
-
+    GridView gridView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         return root;
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        gridView = getView().findViewById(R.id.gridView);
+        try {
+            readCards(getContext());
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        showCards(getContext());
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
+        cards.clear();
         binding = null;
     }
-    private static void readCards(Context context){
+    private void readCards(Context context) throws JSONException {
         File myDir = new File(context.getFilesDir() + "/saved_images");
         File[] files = myDir.listFiles();
-        for(int i = 0;i<files.length;i++){
-            PngReader pngr = new PngReader(files[i]);
+        for (File file : files) {
+            PngReader pngr = new PngReader(file);
             String data = pngr.getMetadata().getTxtForKey("chara");
-            Uri uri = Uri.fromFile(files[i]);
+            Uri uri = Uri.fromFile(file);
+            cards.add(new Card(data,uri));
             pngr.close();
         }
+    }
+    private void showCards(Context context){
+        MainAdapter adapter = new MainAdapter(context,cards);
+        gridView.setAdapter(adapter);
     }
 }
